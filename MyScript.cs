cb665a0dfacbe8e9f72d29a1a8613d865655cff0
Tparker85 <Pawka8855@gmail.com>
2022-11-08 09:49:01 -0700

    first C# commit

---
+++ MyScript.cs
@@ -0,0 +1,19 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terrance
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
